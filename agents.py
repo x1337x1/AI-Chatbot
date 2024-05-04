22012949115a41015459566eee571eea9a1b675a
mohaled <hachemimohammedamine@outlook.com>
2024-05-05 00:11:54 +0100

    agents

--- agents.py
+++ agents.py
@@ -1,4 +1,5 @@
 import os
+import getpass
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
@@ -11,45 +12,50 @@ from controllers.pinecone_controller import PineconeManager
 from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
-
+from langchain.tools.retriever import create_retriever_tool
+from langchain.utilities.tavily_search import TavilySearchAPIWrapper
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain import hub
+from langchain.agents import create_openai_functions_agent
+from langchain.agents import AgentExecutor
 from dotenv import load_dotenv
 load_dotenv()
 
 
-llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+
+llm = ChatOpenAI(api_key=os.getenv('OPENAI_API_KEY'), temperature=0.7)
 pinecone_manager = PineconeManager()
 
-def generate_response_chain_with_history(namespace):
+
+search = TavilySearchAPIWrapper()
+tavily_tool = TavilySearchResults(api_wrapper=search)
+
+def generate_response_chain_with_agents(namespace):
     vector_store = pinecone_manager.get_vectorstore(namespace)
     prompt = ChatPromptTemplate.from_messages([
         ("system", "Answer the user's questions based on the context: {context}"),
         MessagesPlaceholder(variable_name="chat_history"),
         ("user", "{input}")
     ])
-    chain = create_stuff_documents_chain(llm, prompt)
-    retriever = vector_store.as_retriever()
-
-     
-    retriever_prompt = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "{input}"),
-        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
-    ])
-    history_aware_retriever = create_history_aware_retriever(llm, retriever, retriever_prompt)
-    retrieval_chain = create_retrieval_chain(history_aware_retriever, chain)
-    return retrieval_chain
+    retriever = vector_store.as_retriever() ## method that we use to get documents from pinecone
+    retriever_tool = create_retriever_tool(retriever, "langsmith_search", "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!")
+    tools = [retriever_tool, tavily_tool]
+    agent_prompt = hub.pull("hwchase17/openai-functions-agent") ## prompt
+    agent = create_openai_functions_agent(llm, tools, agent_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    return agent_executor
 
 
-def process_chat(chain, question, chat_history):
-    response = chain.invoke({
+def process_chat(agent_executor, question, chat_history):
+    response = agent_executor.invoke({
         "chat_history": chat_history,
         "input": question,
     })
-    return response["answer"]
+    return response
 
 
 if __name__ == "__main__":
-    chain = generate_response_chain_with_history("1337_namespace")
+    agent = generate_response_chain_with_agents("langsmith_namespace")
     # Initialize chat history
     chat_history = []
 
@@ -57,7 +63,7 @@ if __name__ == "__main__":
         user_input = input("You: ")
         if user_input.lower() == 'exit':
             break
-        response = process_chat(chain, user_input, chat_history)
+        response = process_chat(agent, user_input, chat_history)
         chat_history.append(HumanMessage(content=user_input))
-        chat_history.append(AIMessage(content=response))
+        chat_history.append(AIMessage(content= str(response) ))
         print("Assistant:", response)
